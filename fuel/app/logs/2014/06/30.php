c8ad50eaf83301efe9544c55dc45a0e9390eceb3
HeyG <dvlpyeti@HeyG.local>
2014-06-30 15:51:38 +0900

    first commit

--- fuel/app/logs/2014/06/30.php
+++ fuel/app/logs/2014/06/30.php
@@ -4,3 +4,4 @@ ERROR - 2014-06-30 09:51:44 --> Error - ViewModel "\View_Top_404" could not be f
 ERROR - 2014-06-30 09:53:20 --> Error - ViewModel "\View_Top_404" could not be found. in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/viewmodel.php on line 73
 ERROR - 2014-06-30 13:04:02 --> Error - ViewModel "\View_Top_404" could not be found. in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/viewmodel.php on line 73
 ERROR - 2014-06-30 13:08:11 --> Error - ViewModel "\View_Top_404" could not be found. in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/viewmodel.php on line 73
+ERROR - 2014-06-30 15:46:37 --> Error - File "APPPATH/classes/view/top/404.php" does not contain class "View_Top_404" in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/autoloader.php on line 395
