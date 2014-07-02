1359f003ecfc0e5de0c9784d34120b56d8d34ca6
HeyG <dvlpyeti@HeyG.local>
2014-07-02 21:31:50 +0900

    fix the precvious commit

--- fuel/app/logs/2014/07/02.php
+++ fuel/app/logs/2014/07/02.php
@@ -1,3 +1,5 @@
 <?php defined('COREPATH') or exit('No direct script access allowed'); ?>
 
 ERROR - 2014-07-02 19:40:44 --> Error - File "APPPATH/classes/view/top/404.php" does not contain class "View_Top_404" in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/autoloader.php on line 395
+ERROR - 2014-07-02 20:28:12 --> Error - File "APPPATH/classes/view/top/404.php" does not contain class "View_Top_404" in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/autoloader.php on line 395
+ERROR - 2014-07-02 21:14:49 --> Error - File "APPPATH/classes/view/top/404.php" does not contain class "View_Top_404" in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/autoloader.php on line 395
