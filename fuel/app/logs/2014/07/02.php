45d5e495a0b7cd24ae923663ed970668c0a1763e
HeyG <dvlpyeti@HeyG.local>
2014-07-02 20:04:08 +0900

    Fix the top image bag

---
+++ fuel/app/logs/2014/07/02.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-07-02 19:40:44 --> Error - File "APPPATH/classes/view/top/404.php" does not contain class "View_Top_404" in /Users/dvlpyeti/Public/Develop/FuelProjects/eta/fuel/core/classes/autoloader.php on line 395
